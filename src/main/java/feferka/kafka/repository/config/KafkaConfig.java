6af29cdaa2a36c1fd4afbfcb316143d8f401fb40
Pavol Melichárek <melicpav2@eurowag.com>
2025-06-19 13:57:08 +0200

    another refactoring, minimal form

--- src/main/java/feferka/kafka/repository/config/KafkaConfig.java
+++ src/main/java/feferka/kafka/repository/config/KafkaConfig.java
@@ -1,16 +1,12 @@
 package feferka.kafka.repository.config;
 
-import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
-
-import java.util.Map;
 
 @Getter
 @EnableKafka
@@ -19,23 +15,9 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class KafkaConfig {
 
+    private final KafkaStreamsConfiguration configuration;
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
-
     @Value(value = "${spring.kafka.topics.locations}")
     private String locationTopic;
-
-    private final KafkaStreamsConfiguration configuration;
-
-    @PostConstruct
-    void init() {
-        configuration
-                .asProperties()
-                .putAll(Map.of(
-                                StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG, KafkaStreamsErrorHandlers.CustomDeserializationExceptionHandler.class,
-                                StreamsConfig.PROCESSING_EXCEPTION_HANDLER_CLASS_CONFIG, KafkaStreamsErrorHandlers.CustomProcessingExceptionHandler.class,
-                                StreamsConfig.DEFAULT_PRODUCTION_EXCEPTION_HANDLER_CLASS_CONFIG, KafkaStreamsErrorHandlers.CustomProductionExceptionHandler.class
-                        )
-                );
-    }
 }
--- src/main/java/feferka/kafka/repository/repository/KafkaRepository.java
+++ src/main/java/feferka/kafka/repository/repository/KafkaRepository.java
@@ -1,19 +1,25 @@
 package feferka.kafka.repository.repository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StoreQueryParameters;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 
+import java.util.Iterator;
 import java.util.Objects;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @RequiredArgsConstructor
 public abstract class KafkaRepository<K, V> {
@@ -44,6 +50,26 @@ public abstract class KafkaRepository<K, V> {
     }
 
     public Stream<V> findAll() {
-        return KafkaStreamUtils.streamValues(getStore().all());
+        return streamValues(getStore().all());
+    }
+
+    private Stream<V> streamValues(KeyValueIterator<K, V> kvIterator) {
+        val valueIterator = new Iterator<V>() {
+            @Override
+            public boolean hasNext() {
+                return kvIterator.hasNext();
+            }
+
+            @Override
+            public V next() {
+                return kvIterator.next().value;
+            }
+        };
+
+        val spliterator = Spliterators.spliteratorUnknownSize(valueIterator, Spliterator.ORDERED);
+
+        return StreamSupport
+                .stream(spliterator, false)
+                .onClose(kvIterator::close);
     }
 }
